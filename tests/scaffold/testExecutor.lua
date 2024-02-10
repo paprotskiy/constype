3861a04c7d4caf8c739d16c7be42b19a7caf5475
paprotskiy <paprotskiy.roman@gmail.com>
2024-02-10 16:22:43 +0300

    resolve merge error

--- tests/scaffold/testExecutor.lua
+++ tests/scaffold/testExecutor.lua
@@ -3,36 +3,36 @@ local core = require("tests.scaffold.scaffold")
 local pp = require("src.print.pretty")
 
 return {
-   tests = {},
+	tests = {},
 
-   Add = function(self, testFunc)
-      local key = testFunc.name
-      self.tests[key] = testFunc
+	Add = function(self, testFunc)
+		local key = tostring(testFunc)
+		self.tests[key] = testFunc
 
-      return self
-   end,
+		return self
+	end,
 
-   RunAll = function(self)
-      print("run all tests:")
+	RunAll = function(self)
+		print("run all tests:")
 
-      local failed = {}
-      for key, test in pairs(self.tests) do
-         local res = test.exec()
-         local err = core.GetTestErrMessage(res, true)
-         if err ~= nil then
-            table.insert(failed, {
-               test_name = key,
-               Error = err,
-            })
-         end
-         print(res == nil, test.name)
-      end
+		local failed = {}
+		for key, test in pairs(self.tests) do
+			local res = test.exec()
+			local err = core.GetTestErrMessage(res, true)
+			if err ~= nil then
+				table.insert(failed, {
+					test_name = key,
+					Error = err,
+				})
+			end
+			print(res == nil, test.name)
+		end
 
-      if #failed > 0 then
-         print("failed " .. tostring(#failed) .. " test(s):")
-         print(pp.PrettyPrintTests(failed))
-      end
+		if #failed > 0 then
+			print("failed " .. tostring(#failed) .. " test(s):")
+			print(pp.PrettyPrintTests(failed))
+		end
 
-      return self
-   end,
+		return self
+	end,
 }
