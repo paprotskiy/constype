6e49735f9462acb405eaeb173bff222a0c08d83a
SamIlias <samovichilias19life@gmail.com>
2024-07-12 23:47:10 +0300

    #34 implementation

--- src/app/controllers/importAsPlan.lua
+++ src/app/controllers/importAsPlan.lua
@@ -1,14 +1,41 @@
 local view = require("app.ui.view.importAsPlan")
 
-local import_as_plan_controller = function(base_controller_invoke)
+local import_as_plan_controller = function(base_controller_invoke, cfg)
+
+	local function list_files(directory)
+		local files = {}
+		local cmd = 'ls "' .. directory .. '"' .. '| grep txt'
+		local handle = io.popen(cmd)
+
+		if handle then
+			for file in handle:lines() do
+				table.insert(files, {
+						name = file,
+						call = function() end,		
+					})
+			end
+			handle:close()
+		end
+	
+		return files
+	end
+
+	local list = list_files("../../../texts")
 
 	return {
 		load = function()
-			view.load()
+			local val = {}
+			for _, v in ipairs(list) do
+				table.insert(val, {
+					value = v.name,
+				})
+			end
+
+			view:load(cfg, val)
 		end,
 
 		close = function()
-			view.close()
+			view:close()
 		end,
 
 		handle_signal = function(self, atomic_signal)
@@ -22,6 +49,14 @@ local import_as_plan_controller = function(base_controller_invoke)
 
 		default = function(_, signalChar) end,
 
+		["j"] = function(_, _)
+			view:pick_next()
+		end,
+
+		["k"] = function(_, _)
+			view:pick_prev()
+		end,
+
 		--  todo make backspace const
 		[string.char(127)] = function(_, _)
 			base_controller_invoke:bye()
@@ -34,7 +69,13 @@ local import_as_plan_controller = function(base_controller_invoke)
 
 		--  todo make enter const
 		[string.char(10)] = function(_, _)
-			base_controller_invoke:menu()
+			-- local curr = view:get_current()
+			-- for _, v in ipairs(list_files) do
+			-- 	if v.name == curr.value then
+			-- 		v.call()
+			-- 		return
+			-- 	end
+			-- end
 		end,
 	}
 end
--- src/app/ui/view/importAsPlan.lua
+++ src/app/ui/view/importAsPlan.lua
@@ -1,24 +1,106 @@
 local tty = require("app.ui.tty.tty")
+local elementCross = require("app.ui.view.standardComponents.cross")
+local elementList = require("app.ui.view.standardComponents.list")
+
+local function insertIntoTemplate(base, insertion, offsetIdx)
+	local insertionLen = utf8.len(tty.drop_color_wrap(insertion))
+
+	local prefix = base:sub(1, utf8.offset(base, offsetIdx))
+	local suffix = base.sub(base, utf8.offset(base, offsetIdx + insertionLen + 2))
+
+	return prefix .. insertion .. suffix
+end
+
+-- todo must be extracted and tested
+local function trimText(txt, max_length)
+	if utf8.len(txt) > max_length then
+		local truncatedLen = max_length - 3
+		local byte_position = utf8.offset(txt, truncatedLen + 1) - 1
+		return txt:sub(1, byte_position) .. "..."
+	else
+		return txt
+	end
+end
+
+local function render(trimmed_list)
+	local list = trimmed_list:to_lines()
+
+	local template_top = {
+		"╔══════════════════════════════════════════════════╗",
+		"║ ----------------- Import plan ------------------ ║",
+		"╠══════════════════════════════════════════════════╣",
+		"║ ┌──────────────────────────────────────────────┐ ║",
+	}
+
+	local template_row = {
+		"║ │                                              │ ║",
+		"║ ├──────────────────────────────────────────────┤ ║",
+	}
+
+	local template_bottom = {
+		"║ └──────────────────────────────────────────────┘ ║",
+		"╚══════════════════════════════════════════════════╝",
+	}
+
+	local lines = {}
+	table.move(template_top, 1, #template_top, 1, lines)
+	for _, v in ipairs(list) do
+		table.insert(lines, insertIntoTemplate(template_row[1], v, 5))
+		table.insert(lines, template_row[2])
+	end
+	if #lines > #template_top then
+		table.remove(lines, #lines)
+	end
+	table.insert(lines, template_bottom[1])
+	table.insert(lines, template_bottom[2])
+
+	local winsize = tty:wins_size()
+	local offseted = elementCross.offset_row_pool(lines, winsize)
+
+	for _, row in pairs(offseted) do
+		tty.jump(row.X, row.Y)
+		tty.print(row.line)
+	end
+end
+
+local import_list = nil
+local word_max_width = 44
 
 return {
 	close = function()
 		tty.clear_screen()
+		tty.jump(1, 1)
 		os.execute("tput cnorm")
 		os.execute("stty echo -cbreak </dev/tty >/dev/tty 2>&1")
 	end,
 
-	load = function()
+	load = function(self, colorsCfg, list)
 		os.execute("tput civis")
 		os.execute("stty -echo cbreak </dev/tty >/dev/tty 2>&1")
-
 		tty.clear_screen()
 
-		local msg = "Will be implemented later"
-		local winsize = tty:wins_size()
-		local offset_x = (winsize.max_x - #msg) // 2
-		local offset_y = winsize.max_y // 2
+		for idx = 1, #list, 1 do
+			list[idx].value = trimText(list[idx].value, word_max_width)
+		end
 
-		tty.jump(offset_x, offset_y)
-		tty.print(msg)
+		if import_list == nil then
+			import_list = elementList.new_trimmed_list(colorsCfg.Active, colorsCfg.default, list, 4)
+		end
+
+		render(import_list)
+	end,
+
+	pick_prev = function(self)
+		import_list.pick_prev()
+		render(import_list)
+	end,
+
+	pick_next = function(self)
+		import_list.pick_next()
+		render(import_list)
+	end,
+
+	get_current = function(self)
+		return import_list.get_current()
 	end,
 }
